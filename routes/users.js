ee4cddc22ee22811c25b524d4f1f4ba819686f84
stefkudla <stefankudla@gmail.com>
2022-03-16 13:06:41 -0700

    added functionality to update and delete users

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,24 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import {
+  createUser,
+  getUsers,
+  getUser,
+  deleteUser,
+  updateUser,
+} from '../controllers/users.js';
 
 const router = express.Router();
 
-const users = [];
+router.get('/', getUsers);
 
-router.get('/', (req, res) => {
-  res.send(users);
-});
+router.post('/', createUser);
 
-router.post('/', (req, res) => {
-  const user = req.body;
+router.get('/:id', getUser);
 
-  users.push({ ...user, id: uuidv4() });
+router.delete('/:id', deleteUser);
 
-  res.send(`User with the name ${user.firstName} added to the database!`);
-});
-
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const foundUser = users.find((user) => user.id === id);
-
-  res.send(foundUser);
-});
+router.patch('/:id', updateUser);
 
 router.delete;
 

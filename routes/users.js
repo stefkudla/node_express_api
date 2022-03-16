5eb3e40c641e7e831a4e2baf198d9ac653371a55
stefkudla <stefankudla@gmail.com>
2022-03-16 10:33:30 -0700

    POST request functionality

--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,30 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 25,
-  },
-  {
-    firstName: 'Jane',
-    lastName: 'Doe',
-    age: 24,
-  },
-];
+const users = [];
 
 router.get('/', (req, res) => {
-  console.log(users);
-
   res.send(users);
 });
 
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  const user = req.body;
+
+  users.push({ ...user, id: uuidv4() });
+
+  res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.delete;
 
 export default router;
